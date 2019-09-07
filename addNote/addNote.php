56034172d04b7f37eeafa6a5f244b62e359d96ec
Владислав <kaktyc_real_ii@mail.ru>
2019-09-07 22:36:27 +0300

    project without architecture

--- addNote/addNote.php
+++ addNote/addNote.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'rb/rb.php';
+require '../rb/rb.php';
 R::setup( 'mysql:host = 127.0.0.1;
  dbname=notebook',
  'root',
@@ -9,20 +9,24 @@ R::setup( 'mysql:host = 127.0.0.1;
 if (!R::testConnection()) {
 	exit ('Нет подключения к базе данных!');
 }
-
 // CREATE
 $comment = R::dispense('comment');
 
 $comment->name = $_POST['name'];
 $comment->message = $_POST['message'];
 $comment->date = date("l d F Y");
+$comment->date_calendar = date("m/d/y");
 $comment->time = date("h:i:s");
  R::store($comment);
 
 // Считывает сколько записей имеется в общем
-$zapros = R::exec('SELECT * FROM comment WHERE id');
-print_r($zapros);
+$all_id = R::exec('SELECT * FROM comment WHERE id');
+print_r($all_id);
 
-$comments = R::loadAll('comment', $zapros);
+$comments = R::loadAll('comment', $all_id);
+foreach ($comments as $comment){
+  echo $comment->id.'<br>';
+  echo $comment->name.'<br>';
+}
 
  ?>
